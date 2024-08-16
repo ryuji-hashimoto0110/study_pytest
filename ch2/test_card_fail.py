977e6a4cbe6aa4a94db8e625251608c0ddfc9adc
ryuji-hashimoto0110 <ryujihashimoto@hashimotoryuujinoMacBook-Pro.local>
2024-08-16 11:54:41 +0900

    use $ pytest -vv test_card_fail.py to see the detailed traceback.

--- ch2/test_card_fail.py
+++ ch2/test_card_fail.py
@@ -1,6 +1,6 @@
 from cards import Card
 
-def test_equality_fail():
+def test_equality_fail(): # use $ pytest -vv test_card_fail.py to see the detailed traceback.
     c1 = Card("sit there", "brian")
     c2 = Card("do something", "okken")
     assert c1 == c2
